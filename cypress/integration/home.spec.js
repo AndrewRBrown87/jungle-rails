d018d4a66fab6d27ed03fdfd0d02f1283d26fa15
Andrew Brown <andrew.robert.brown@gmail.com>
2023-04-27 19:53:31 +0000

    Add feature/testing-home-page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+describe('jungle app', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });  
+  
+})
